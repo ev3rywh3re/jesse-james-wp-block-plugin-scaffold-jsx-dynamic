b2db0669bf886a04028062f28de4f0b21939de80
ev3rywh3re <jess@funroe.net>
2024-08-12 11:31:17 -0500

    Ok. A sort of working scaffold.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
+const path = require('path');
+
 module.exports = {
-	entry: './src/block.js',
-	output: {
-		path: __dirname,
-		filename: 'build/block.js',
-	},
-	module: {
-		loaders: [
-			{
-				test: /.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
+  entry: './src/index.js', // Entry point for your block's JavaScript
+  output: {
+    path: path.resolve(__dirname, 'build'), // Output directory for bundled files
+    filename: '[name].js' // Output filename pattern
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Match JavaScript files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel loader for transpilation
+        }
+      },
+      {
+        test: /\.css$/, // Match CSS files
+        use: ['style-loader', 'css-loader'] // Use style-loader and css-loader for CSS handling
+      },
+      {
+        test: /\.json$/, // Match JSON files
+        use: 'file-loader' // Use FileLoader to handle JSON files
+      }
+    ]
+  },
+  
 };
